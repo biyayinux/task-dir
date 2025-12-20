1a08d18c8bf94beb0031473232dfefc07db3adcb
biyayinux <joshu404biya@gmail.com>
2025-12-20 15:13:21 +0100

    Navigation vers la page de login

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,17 @@
 <template>
-  <h1 class="p-4 py-8 text-center text-2xl">
-    Système interne de gestion réservé aux membres officiels de la start-up
-    MKDIR
-  </h1>
+  <div class="text-center">
+    <h1 class="p-4 py-8 text-2xl">
+      Système interne de gestion réservé aux membres officiels de la start-up
+      MKDIR
+    </h1>
+
+    <NuxtLink
+      href="/login"
+      class="inline-block rounded-lg bg-[#24455C] px-6 py-2 text-white"
+    >
+      Se connecter
+    </NuxtLink>
+  </div>
 </template>
 
 <script setup>
---
+++ app/pages/login.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="p-4 py-8 text-center text-2xl">Connexion</h1>
+</template>
