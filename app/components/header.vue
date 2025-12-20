a3f75aff81f483cb8be92a7e5fbb35b99d135515
biyayinux <joshu404biya@gmail.com>
2025-12-20 13:34:36 +0100

    Lucide Icons

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,5 +1,6 @@
 <template>
-  <header class="p-4 text-center">
-    <h1 class="text-2xl">TASKDIR</h1>
+  <header class="flex justify-center gap-2 p-4">
+    <LucideClipboardCheck class="h-7 w-7" />
+    <h1 class="text-2xl">TaskDir</h1>
   </header>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['./assets/styles/default.css'],
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  modules: ['nuxt-lucide-icons']
 })
