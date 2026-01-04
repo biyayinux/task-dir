a02696199f71ac9d3541f367ddfb0c42ed569116
biyayinux <joshu404biya@gmail.com>
2026-01-04 22:58:03 +0100

    Ajout du Style UI

---
+++ app/components/ui/button.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="flex items-center gap-2 rounded-lg bg-black px-4 py-2 text-white transition-colors duration-500 ease-in-out hover:bg-gray-600 dark:bg-white dark:text-black dark:hover:bg-gray-400"
+  >
+    <slot />
+  </button>
+</template>
--- app/pages/login.vue
+++ app/pages/login.vue
@@ -3,43 +3,37 @@ const config = useRuntimeConfig()
 const route = useRoute()
 
 onMounted(async () => {
-  // On vérifie s'il y a un token dans l'URL
+  // Récupère le token depuis l'URL après la connexion Google
   const token = route.query.token
 
   if (token) {
-    // On enregistre le token dans le navigateur
+    // Sauvegarde le token et redirige vers l'accueil
     localStorage.setItem('auth_token', token)
-
-    // On redirige immédiatement vers l'accueil sans le token dans l'URL
     await navigateTo('/')
     return
   }
 
-  // Vérifie si le token est encore valide pour rediriger vers la page de profil
+  // Vérifie si un token existe déjà dans le navigateur
   const existingToken = localStorage.getItem('auth_token')
   if (existingToken) {
+    // Vérifie si le token est toujours valide auprès du backend
     const res = await fetch(`${config.public.backendUrl}/admin/profile`, {
       headers: { Authorization: `Bearer ${existingToken}` }
     })
+    // Si valide, redirection automatique vers l'accueil
     if (res.ok) await navigateTo('/')
   }
 })
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col items-center justify-center">
-    <h1 class="p-4 py-8 text-center text-2xl font-bold">
-      Connexion Administrateur
-    </h1>
-    <a
-      :href="`${config.public.backendUrl}/auth/google`"
-      class="flex items-center gap-3 rounded-lg border px-6 py-2 transition hover:bg-gray-50"
-    >
-      <img
-        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-        width="20"
-      />
-      Continuer avec Google
+  <div class="flex min-h-screen flex-col items-center justify-center gap-10">
+    <UiTitle>Connexion Administrateur</UiTitle>
+    <a :href="`${config.public.backendUrl}/auth/google`" class="block">
+      <UiButton>
+        <img src="/images/icons/google.svg" width="20" alt="Google" />
+        <span>Continuer avec Google</span>
+      </UiButton>
     </a>
   </div>
 </template>
