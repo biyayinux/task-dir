5d4e61689c09ce433dbbaa7bbdd5207103023ab1
biyayinux <joshu404biya@gmail.com>
2026-01-06 00:08:09 +0100

    Layout uniquement pour le dossier pseudo

---
+++ app/pages/[pseudo].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// Utilise le layout 'pseudo' pour toutes les pages /[pseudo]
+definePageMeta({
+  layout: 'pseudo'
+})
+</script>
+
+<template>
+  <NuxtPage />
+</template>
