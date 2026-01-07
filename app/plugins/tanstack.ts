32c52a778aa8473053cc54692de4b1adfd97c2ae
biyayinux <joshu404biya@gmail.com>
2026-01-07 15:17:56 +0100

    Commantaires au fichier tanstack.ts

--- app/plugins/tanstack.ts
+++ app/plugins/tanstack.ts
@@ -6,22 +6,26 @@ import {
 } from '@tanstack/vue-query'
 
 export default defineNuxtPlugin((nuxtApp) => {
+  // Création du client avec un cache de 5 minutes
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 1000 * 60 * 5 // Les données sont considérées 'neuves' pendant 5 minutes
+        staleTime: 1000 * 60 * 5
       }
     }
   })
 
+  // Installation du plugin dans l'application Nuxt
   nuxtApp.vueApp.use(VueQueryPlugin, { queryClient })
 
+  // Capture l'état du cache pour l'envoyer au navigateur
   if (import.meta.server) {
     nuxtApp.hooks.hook('app:rendered', () => {
       nuxtApp.payload.vueQueryState = dehydrate(queryClient)
     })
   }
 
+  // Réutilise les données reçues du serveur (évite le double fetch)
   if (import.meta.client) {
     hydrate(queryClient, nuxtApp.payload.vueQueryState)
   }
