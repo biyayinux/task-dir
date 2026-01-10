9dfb3a97bd64eee46dc611c06901676d6e5af1fb
biyayinux <joshu404biya@gmail.com>
2026-01-10 12:44:46 +0100

    Ajout de Tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,13 @@
 <template>
-  <h1>TaskDir</h1>
-  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, recusandae commodi cumque maxime dolorem excepturi ad, ipsam accusantium quibusdam nesciunt officiis inventore unde dignissimos minus laborum laboriosam optio atque ipsa.</p>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-white text-black text-center px-4">
+    <h1 class="text-4xl font-bold mb-4">
+      TaskDir
+    </h1>
+    <p class="max-w-xl text-base">
+      🚀💻 Application full-stack développée avec Nuxt.js, dédiée à la gestion des tâches 📝📊
+      des membres 👥🤝 de la start-up MKDIR 🏢🚀.
+      Elle améliore la collaboration, l’organisation du travail et la productivité
+      grâce à une interface moderne et intuitive ⚡📈.
+    </p>
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   // Modules
   modules: ['@nuxt/eslint'],
@@ -6,11 +7,24 @@ export default defineNuxtConfig({
   // Outils de développement
   devtools: { enabled: true },
 
+  // Déclaration du fichier CSS global
+  css: ['/assets/css/main.css'],
+
   // Compatibilité Nuxt 4
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2025-07-15',
+
+  // Plugin Tailwind pour Vite
+  vite: {
+    build: {
+      sourcemap: false, // Désactive la génération des sourcemaps
+    },
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   // Configuration ESLint
   eslint: {
